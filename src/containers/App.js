def7400703f3bb954500332b6bbedf985a007d84
barakjobs <nirajprasad2703@gmail.com>
2019-11-23 19:55:07 +0530

    final project

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,26 +1,90 @@
-import React from 'react';
-import logo from './../logo.svg';
-import './../App.css';
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { selectReddit, fetchPostsIfNeeded, invalidateReddit } from '../actions';
+import Picker from '../components/Picker';
+import Posts from '../components/Posts';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+class App extends Component {
+  // static PropTypes = {
+  //   selectReddit: PropTypes.string.isRequired,
+  //   posts: PropTypes.array.isRequired,
+  //   isFetching: PropTypes.bool.isRequired,
+  //   lastUpdated: PropTypes.number,
+  //   dispatch: PropTypes.func.isRequired
+  // }
+
+  componentDidMount() {
+    const {  dispatch, selectedReddit } = this.props
+    dispatch(fetchPostsIfNeeded(selectedReddit))
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.selectedReddit !== this.props.selectedReddit) {
+      const { dispatch, selectedReddit } = nextProps
+      dispatch(fetchPostsIfNeeded(selectedReddit))
+    }
+  }
+
+  handleChange = nextReddit => {
+    this.props.dispatch(selectReddit(nextReddit))
+  }
+
+  handleRefreshClick = e => {
+    e.preventDefault()
+
+    const { dispatch, selectedReddit } = this.props
+    dispatch(invalidateReddit(selectedReddit))
+    dispatch(fetchPostsIfNeeded(selectedReddit))
+  }
+
+  render() {
+    const { selectedReddit, posts, isFetching, lastUpdated } = this.props
+    const isEmpty = posts.length === 0;
+    return (
+      <div>
+        <Picker value={selectedReddit}
+                onChange={this.handleChange}
+                options={[ 'reactjs', 'frontend']} />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {lastUpdated &&
+            <span>
+              Last Modified at {new Date(lastUpdated).toLocaleTimeString()}.
+              {' '}
+            </span>}
+          }
+          {!isFetching &&
+            <button onClick={this.handleRefreshClick}>Refresh</button>
+          }
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        {isEmpty
+          ? (isFetching ? <h2>Loading...</h2> : <h2>Empty.</h2>)
+          : <div style={{ opacity: isFetching ? 0.5 : 1 }}>
+              <Posts posts={posts} />
+            </div>
+        }
+      </div>
+    )
+  }
 }
 
-export default App;
+const mapStateToProps = state => {
+  const { selectedReddit, postsByReddit } = state;
+  const {
+    isFetching,
+    lastUpdated,
+    items: posts
+  } = postsByReddit[selectedReddit] || {
+    isFetching: true,
+    items: []
+  }
+
+  return {
+    selectedReddit,
+    posts,
+    isFetching,
+    lastUpdated
+  }
+}
+
+export default connect(mapStateToProps)(App);
